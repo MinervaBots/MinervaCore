e5ef6568b1df218161fee9350e17065656665824
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-12-07 23:01:48 -0300

    BUILD: Atualizando favicon

--- minervacore/docusaurus.config.js
+++ minervacore/docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'MinervaCore',
   tagline: 'Documentation for MinervaCore',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/minervacore-logo.jpg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
