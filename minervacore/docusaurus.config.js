f96aab6259c6d4053440ed4fb5b541c7c24cd9ad
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-12-08 06:08:04 -0300

    ADD: Automação de deploy GitHub Actions CI

--- minervacore/docusaurus.config.js
+++ minervacore/docusaurus.config.js
@@ -19,25 +19,16 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // url: 'https://minervabots.github.io', 
-  // baseUrl: '/minervacore/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'MinervaBots', // Usually your GitHub org/user name.
-  projectName: 'MinervaCore', // Usually your repo name.
+  // Configurações para o GitHub Pages
+  url: 'https://MinervaBots.github.io',
+  baseUrl: '/MinervaCore/',
+  organizationName: 'MinervaBots',
+  projectName: 'MinervaCore',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'pt-BR',
     locales: ['pt-BR'],
@@ -92,7 +83,7 @@ const config = {
             label: 'GitHub',
             position: 'right',
           },
-          
+
           {
             href: 'https://gitlab.com/MinervaBots',
             label: 'GitLab',
