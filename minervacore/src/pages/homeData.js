253172735cd5c46be195fee7fbfceb940625ce65
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-12-16 06:07:21 -0300

    REFACTOR: Tirando a lista de botões do arquivo principal para um arquivo separado
    Estava ficando muito grande o código do arquivo principal e desorganizado

---
+++ minervacore/src/pages/homeData.js
@@ -0,0 +1,190 @@
+// Arquivo para armazenar a lista de dados do site
+
+/* ==========================================================================
+                              DADOS DO SITE
+   ========================================================================== 
+   
+   O templante para criar novos botões é:
+    - Escolher uma categoria (programacao, arquitetura, eletronica)
+    - Adicionar um novo objeto no array 'items' da subcategoria desejada
+    - Seguir o modelo:
+        { 
+          title: 'NOME DO TÓPICO',
+          link: '/docs/caminho/para/o/topico',
+          iconSrc: '/img/icons/nome-do-icone.svg' 
+        }
+   
+   Lembre-se de criar o ícone SVG correspondente na pasta 'static/img/icons/'.
+   Idealmente, os ícones devem ser simples e representativos do tópico.
+   A maioria dos ícones podem ser encontrados nos seguintes sites:
+    - simpleicons.org (para logos de empresas e tecnologias)
+    - freesvgicons.com (para ícones genéricos)
+*/
+const SiteData = {
+
+/* --------------- PROGRAMAÇÃO --------------- */
+
+  programacao: [
+    {
+      title: 'Linguagens',
+      items: [
+        { title: 'C++', link: '/docs/programacao/cpp', iconSrc: '/img/icons/cpp.svg' },
+        { title: 'Python', link: '/docs/programacao/python', iconSrc: '/img/icons/python.svg' },
+        { title: 'Dart & Flutter', link: '/docs/programacao/dart-flutter', iconSrc: '/img/icons/dart.svg' },
+        { title: 'Markdown', link: '/docs/programacao/markdown', iconSrc: '/img/icons/markdown.svg' },
+        { title: 'NXC', link: '/docs/programacao/nxc', iconSrc: '/img/icons/nxc.svg' },
+      ]
+    },
+    {
+      title: 'Firmware & Sistemas Operacionais',
+      items: [
+        { title: 'Arduino', link: '/docs/programacao/arduino', iconSrc: '/img/icons/arduino.svg' },
+        { title: 'Espressif IDF', link: '/docs/programacao/espressif', iconSrc: '/img/icons/espressif.svg' },
+        { title: 'FreeRTOS', link: '/docs/programacao/freertos', iconSrc: '/img/icons/freertos.svg' },
+        { title: 'PlatformIO', link: '/docs/programacao/platformio', iconSrc: '/img/icons/platformio.svg' },
+        { title: 'ROS 2', link: '/docs/programacao/ros', iconSrc: '/img/icons/ros.svg' },
+      ]
+    },
+    {
+      title: 'Robótica & Algoritmos',
+      items: [
+        { title: 'Sistemas de Controle', link: '/docs/programacao/sistemas-controle', iconSrc: '/img/icons/robotic-arm.svg' },
+        { title: 'Metaheurísticas', link: '/docs/programacao/metaheuristicas', iconSrc: '/img/icons/smart-optimization.svg' },
+      ]
+    },
+    {
+        title: 'Comunicação',
+        items: [
+          { title: 'JSON', link: '/docs/programacao/json', iconSrc: '/img/icons/json.svg' },
+          { title: 'Wi-Fi', link: '/docs/programacao/wifi', iconSrc: '/img/icons/wifi.svg' },
+          { title: 'Bluetooth', link: '/docs/programacao/bluetooth', iconSrc: '/img/icons/bluetooth.svg' },
+        ]
+    },
+    {
+      title: 'Ferramentas & Simuladores',
+      items: [
+        { title: 'Git', link: '/docs/programacao/git', iconSrc: '/img/icons/git.svg' },
+        { title: 'Wokwi', link: '/docs/programacao/wokwi', iconSrc: '/img/icons/wokwi.svg' },
+      ]
+    }
+  ],
+
+/* --------------- ARQUITETURA DE HARDWARE --------------- */
+
+  arquitetura: [
+    {
+      title: 'Conceitos de Hardware',
+      items: [
+        { title: 'Memória & Armazenamento', link: '/docs/arquitetura/memoria', iconSrc: '/img/icons/memory.svg' },
+        { title: 'Clock & Interrupções', link: '/docs/arquitetura/clock', iconSrc: '/img/icons/chip.svg' },
+        { title: 'Comunicação Serial', link: '/docs/arquitetura/serial', iconSrc: '/img/icons/chip.svg' },
+      ]
+    },
+    {
+      title: 'Microcontroladores',
+      items: [
+        { title: 'ESP-32', link: '/docs/arquitetura/esp32', iconSrc: '/img/icons/espressif.svg' },
+      ]
+    }
+  ],
+
+/* --------------- ELETRÔNICA --------------- */
+
+  eletronica: [
+    {
+      title: 'Componentes Passivos',
+      items: [
+        { title: 'Resistores', link: '/docs/eletronica/componentes/resistores', iconSrc: '/img/icons/resistor.svg' },
+        { title: 'Capacitores', link: '/docs/eletronica/componentes/capacitores', iconSrc: '/img/icons/capacitor.svg' },
+        { title: 'Indutores', link: '/docs/eletronica/componentes/indutores', iconSrc: '/img/icons/inductor.svg' },
+      ]
+    },
+
+    {
+      title: 'Componentes Semicondutores',
+      items: [
+        { title: 'Diodos', link: '/docs/eletronica/componentes/diodos', iconSrc: '/img/icons/diode.svg' },
+        { title: 'Transistores BJT', link: '/docs/eletronica/componentes/transistores', iconSrc: '/img/icons/transistor.svg' },
+        { title: 'MOSFETs', link: '/docs/eletronica/componentes/mosfets', iconSrc: '/img/icons/transistor.svg' },
+      ]
+    },
+
+    {
+      title: 'Componentes Lógicos',
+      items: [
+        { title: 'Multiplexadores', link: '/docs/eletronica/componentes/multiplexadores', iconSrc: '/img/icons/logic-diagram.svg' },
+      ]
+    },
+
+    {
+      title: 'Sistemas de Energia',
+      items: [
+        { title: 'Baterias', link: '/docs/eletronica/energia/baterias', iconSrc: '/img/icons/battery.svg' },
+        { title: 'Reguladores de Tensão', link: '/docs/eletronica/energia/reguladores', iconSrc: '/img/icons/voltage.svg' },
+        { title: 'Circuitos de Proteção', link: '/docs/eletronica/energia/protecao', iconSrc: '/img/icons/shield.svg' },
+      ]
+    },
+
+    {
+      title: 'Controle de Atuação e Motores',
+      items: [
+        { title: 'Motores DC & Brushless', link: '/docs/eletronica/atuacao/motores', iconSrc: '/img/icons/motor.svg' },
+        { title: 'Drivers Ponte H', link: '/docs/eletronica/atuacao/drivers', iconSrc: '/img/icons/chip.svg' },
+        { title: 'ESC', link: '/docs/eletronica/atuacao/esc', iconSrc: '/img/icons/esc.svg' },
+        { title: 'Servomotores', link: '/docs/eletronica/atuacao/servos', iconSrc: '/img/icons/robotic-gripper.svg' },
+      ]
+    },
+
+    {
+      title: 'Microcontroladores & Microprocessadores',
+      items: [
+        { title: 'ESP-32', link: '/docs/eletronica/microcontroladores/esp32', iconSrc: '/img/icons/espressif.svg' },
+      ]
+    },
+
+    {
+      title: 'Sinais & Comunicação',
+      items: [
+        { title: 'Análogico vs Digital', link: '/docs/eletronica/sinais/analogico-digital', iconSrc: '/img/icons/signal.svg' },
+        { title: 'PWM', link: '/docs/eletronica/sinais/pwm', iconSrc: '/img/icons/signal.svg' },
+        { title: 'I2C', link: '/docs/eletronica/sinais/i2c', iconSrc: '/img/icons/signal.svg' },
+        { title: 'SPI', link: '/docs/eletronica/sinais/spi', iconSrc: '/img/icons/signal.svg' },
+        { title: 'UART', link: '/docs/eletronica/sinais/uart', iconSrc: '/img/icons/signal.svg' },
+      ]
+    },
+
+    {
+      title: 'PCB',
+      items: [
+        { title: 'Altium Designer', link: '/docs/eletronica/altium', iconSrc: '/img/icons/altium.svg' },
+      ]
+    },
+
+    {
+      title: 'Sensores',
+      items: [
+        { title: 'Infravermelho', link: '/docs/eletronica/sensores/infravermelho', iconSrc: '/img/icons/infrared.svg' },
+        { title: 'Ultrassom', link: '/docs/eletronica/sensores/ultrassom', iconSrc: '/img/icons/ultrasound.svg' },
+        { title: 'IMU', link: '/docs/eletronica/sensores/imu', iconSrc: '/img/icons/chip.svg' },
+        { title: 'Encoders', link: '/docs/eletronica/sensores/encoders', iconSrc: '/img/icons/encoder.svg' },
+      ]
+    },
+
+    {
+      title: 'Prática',
+      items: [
+        { title: 'Soldagem', link: '/docs/eletronica/pratica/soldagem', iconSrc: '/img/icons/soldering.svg' },
+        { title: 'Crimpagem', link: '/docs/eletronica/pratica/crimpagem', iconSrc: '/img/icons/connector.svg' },
+      ]
+    },
+
+    {
+      title: 'Eletrônica Geral',
+      items: [
+        { title: 'Controles', link: '/docs/eletronica/geral/controles', iconSrc: '/img/icons/controller.svg' },
+      ]
+    }
+  ]
+};
+
+export default SiteData;
